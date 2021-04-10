6874f9863f0683715227faf69b789d14b4a13a9c
Ze-Victor <josevictorjose8@gmail.com>
2021-04-10 06:21:22 -0300

    Tela Editar Atualizada

--- src/br/ufrn/imd/controle/TelaCadastroEventoController.java
+++ src/br/ufrn/imd/controle/TelaCadastroEventoController.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
@@ -37,7 +38,9 @@ public class TelaCadastroEventoController {
     void avançarCadastro(ActionEvent event) throws IOException {
     	String tipo = selectorTipoEvento.getValue();
     	if(tipo == null) {
-    		labelDefault.setVisible(true);
+    		Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setContentText("SELECIONE UM TIPO DE EVENTO");
+			alert.show();
     	}else if(tipo == "Diário"){
     		eventoDiario();
     		//clienteStage.close();
